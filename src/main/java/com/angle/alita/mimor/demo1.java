c2e5241353b6973643cf0b36676b7a376d9306dd
saber-opensource <huxueyan101@gmail.com>
2019-03-19 23:36:06 +0800

    Initial commit

---
+++ src/main/java/com/angle/alita/mimor/demo1.java
@@ -0,0 +1,15 @@
+package com.angle.alita.mimor;
+
+public class demo1 {
+
+    public static void main(String[] args) {
+        Long aLong = 1231231L;
+
+        System.out.println(aLong.toString());
+        System.out.println(aLong.hashCode());
+
+    }
+    static enum demo{
+
+    }
+}
---
+++ src/test/java/com/angle/alita/AlitaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.angle.alita;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlitaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
